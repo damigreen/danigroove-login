4c402c917aee4b0f963b1aff3243c3bfb4f9a79e
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-16 18:13:00 +0100

    SQL information

---
+++ project/-- Data.sql
@@ -0,0 +1,13 @@
+-- Database is an organized collection of structured data.
+-- DBMS is a software used for managing database
+-- Data definition
+-- Data updation
+-- data retreival 
+-- user administration
+
+-- Query is used to extract data from a database and converts it into a human readable form
+-- Every table has a column and every column has a name
+-- Column are vertical section of the table
+-- Table has a row which is the horizontal information in the table. They are a single information about on single person
+-- Field - intersection between a row and a column
+-- SELECT * FROM User WHERE id=1
