fcc9a2601146a417666fdc66bba5c251bee08797
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-16 18:04:12 +0100

    file form

---
+++ superglobals/files.php
@@ -0,0 +1,5 @@
+
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+          <input type="file" name="file">
+          <button type="submit" name="submit" value="submit">SUBMIT</button>
+        </form>
