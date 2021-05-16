b66e7fa29131e00a694d073025f8a1555057e3cf
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-16 18:13:43 +0100

    Application layout

---
+++ project/includes/footer.php
@@ -0,0 +1,17 @@
+<?php
+
+  require_once 'database.php';
+
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MYSQL Database Connection</title>
+  </head>
+  <body>
+    <footer>
+      <p>This is my footer of the login and register form</p>
+    </footer>
+  </body>
+</html>
---
+++ project/includes/header.php
@@ -0,0 +1,24 @@
+<?php
+  session_start();
+  require_once 'database.php';
+  require_once 'register-inc.php';
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>MYSQL Database Connection</title>
+  </head>
+  <body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+      </ul>
+    </nav>
+  </header>
+  </body>
+</html> 
