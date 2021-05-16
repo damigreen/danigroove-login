498e3ed01f3b620776abc7435e6842331acdd8d8
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-16 18:09:27 +0100

    operators

---
+++ operators/arithmetic.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Operators</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Operators</h1>";
+
+            $x = 5;
+            echo $x * 5;
+            echo "<br>";
+
+            $str = "Dami is a boy";
+            $str .= " because is 29 years old";
+            echo $str; 
+            echo "<br>";
+            
+            echo(1<=>2); echo"<br>";
+            echo(2<=>2);echo"<br>";
+            echo(4<=>1);echo"<br>";
+
+        ?>
+    </body>
+</html>
